8a1af6ca6d1f418473d60cf415dae78774964ee4
leinore <116540768+leinore@users.noreply.github.com>
2022-12-09 10:45:42 +0300

    add task_50

---
+++ task_50/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] elements = new int [5,5];
+FillArrayRandom(elements);
+
+if (n > elements.GetLength(0) || m > elements.GetLength(1))
+{
+    Console.WriteLine("Такого числа в массиве нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента ({n},{m}) равно {elements[n-1,m-1]}");
+}
+
+PrintArray(elements);
+
+void FillArrayRandom(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {        
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix [i,j] = new Random().Next(1, 10);
+            }   
+        }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }   
+        Console.WriteLine(); 
+    }
+}
